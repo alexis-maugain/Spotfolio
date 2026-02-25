1ddf5ab4ddd1c379490ca57dc20b5f8b1094b85d
Alexis Maugain <alexis-maugain@users.noreply.github.com>
2026-02-25 17:50:10 +0100

    add

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -4,9 +4,9 @@ export function LoadingScreen() {
       <div className="flex flex-col items-center gap-8">
         {/* Logo animé */}
         <div className="w-20 h-20 bg-gradient-to-br from-green-400 to-green-600 rounded-full flex items-center justify-center animate-pulse">
-          <svg width="48" height="48" viewBox="0 0 24 24" fill="white">
+            <svg width="48" height="48" viewBox="0 0 24 24" fill="white" style={{ transform: "rotate(-90deg)" }}>
             <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4.5 14.5c-.2.3-.5.4-.8.4-.2 0-.3 0-.5-.1-2.4-1.5-5.4-1.8-9-.9-.4.1-.8-.2-.9-.6-.1-.4.2-.8.6-.9 4-.9 7.3-.5 10.1 1.1.4.2.5.7.3 1zm1.1-2.5c-.2.4-.7.5-1.1.3-2.8-1.7-7-2.2-10.3-1.2-.5.2-1-.1-1.2-.6-.2-.5.1-1 .6-1.2 3.8-1.1 8.4-.6 11.6 1.4.4.2.6.7.4 1.1zm.1-2.6c-3.4-2-9-2.2-12.2-1.2-.6.2-1.2-.2-1.4-.8-.2-.6.2-1.2.8-1.4 3.7-1.1 10.1-.9 14.1 1.4.5.3.7 1 .4 1.5-.3.5-1 .7-1.5.4z"/>
-          </svg>
+            </svg>
         </div>
         
         {/* Barre de chargement */}
